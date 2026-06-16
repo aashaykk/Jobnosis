8fd3539e9798ec06c2f92fbf065ee68e02c5b3c4
aashaykk <aashaykamble911@gmail.com>
2026-06-16 15:53:37 +0530

    feat: enhance authentication flow with navigation and loading states in Login and Register components

--- frontend/src/features/auth/pages/Register.jsx
+++ frontend/src/features/auth/pages/Register.jsx
@@ -1,14 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useNavigate, Link } from 'react-router'
+import { useAuth } from '../hooks/useAuth'
 
 const Register = () => {
 
   const naviagte = useNavigate()
+  const [username, setUsername] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const {loading, handleRegister} = useAuth()
 
   const handleSubmit = (e) => {
     e.preventDefault()
+    await handleRegister({username, email, password})
+    naviagte('/')
   }
 
+  if(loading) {
+    return (<main><h1>Loading.....</h1></main>)
+  }
 
   return (
     <main>
@@ -18,15 +29,21 @@ const Register = () => {
         <form onSubmit={handleSubmit}>
           <div className="input-group">
             <label htmlFor="username">Username</label>
-            <input type="text" id="username" name="username" placeholder='Enter the username'/>
+            <input 
+            onChange={(e)=>{setUsername(e.target.value)}}
+            type="text" id="username" name="username" placeholder='Enter the username'/>
           </div>
           <div className="input-group">
             <label htmlFor="email">Email</label>
-            <input type="text" id="email" name="email" placeholder='Enter the email address'/>
+            <input 
+            onChange={(e) => {setEmail(e.target.value)}}
+            type="text" id="email" name="email" placeholder='Enter the email address'/>
           </div>
           <div className="input-group">
             <label htmlFor="password">Password</label>
-            <input type="text" id="password" name="password" placeholder='Enter the password'/>
+            <input 
+            onChange={(e)=>{setPassword(e.target.value)}}
+            type="text" id="password" name="password" placeholder='Enter the password'/>
           </div>
 
           <button className='button primary-button'>Register</button>
